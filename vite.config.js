fa57c1cd35eef6cc1537eafb129a7cce9da712ce
NewTone-Home <ntone4202@gmail.com>
2026-07-24 23:48:32 -0700

    build: support GitHub Pages preview paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const githubPagesBase = process.env.GITHUB_PAGES === 'true'
+  ? '/NewTone-Home/'
+  : '/'
+
 export default defineConfig({
+  base: githubPagesBase,
   plugins: [react()],
 })
