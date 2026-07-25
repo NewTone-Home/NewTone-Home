b56b3ea007eb73d5fc34ee524aea66f6a33b3b04
NewTone-Home <ntone4202@gmail.com>
2026-07-24 23:43:29 -0700

    refactor: load the Phaser Center only when entering Center

--- src/views/Center.jsx
+++ src/views/Center.jsx
@@ -1,1 +1,13 @@
-export { default } from '../center-next/CenterExperience'
+import { lazy, Suspense } from 'react'
+
+const CenterExperience = lazy(() => import('../center-next/CenterExperience'))
+
+function Center() {
+  return (
+    <Suspense fallback={<main className="center-next center-next--loading">正在展开两层世界…</main>}>
+      <CenterExperience />
+    </Suspense>
+  )
+}
+
+export default Center
